8ec35e3993e8c1c5b287c8a58f0bd97a20787bc7
vagrant <vagrant@kleer-csd-box>
2017-09-29 10:05:37 -0300

    Se agregaron las reglas de juego faltantes y las de empate

--- spec/PiedraPapelTijera_spec.rb
+++ spec/PiedraPapelTijera_spec.rb
@@ -19,5 +19,54 @@ describe "PiedraPapelYTijera" do
      juego.ganador.should == "Gana Computadora"
    end
 
+ it "Humano elige tijera, computadora elige papel, gana tijera" do
+     juego = Juego.new
+     juego.humano.setResultado("Tijera")
+     juego.maquina.setResultado("Papel")
+     juego.ganador.should == "Gana Humano"
+   end
+
+ it "Humano elige papel, computadora elige tijera, gana tijera" do
+     juego = Juego.new
+     juego.humano.setResultado("Papel")
+     juego.maquina.setResultado("Tijera")
+     juego.ganador.should == "Gana Computadora"
+   end
+
+ it "Humano elige piedra, computadora elige tijera, gana piedra" do
+     juego = Juego.new
+     juego.humano.setResultado("Piedra")
+     juego.maquina.setResultado("Tijera")
+     juego.ganador.should == "Gana Humano"
+   end
+
+ it "Humano elige piedra, computadora elige papel, gana papel" do
+     juego = Juego.new
+     juego.humano.setResultado("Piedra")
+     juego.maquina.setResultado("Papel")
+     juego.ganador.should == "Gana Computadora"
+   end
+
+ it "Humano elige piedra, computadora elige piedra, empatan" do
+     juego = Juego.new
+     juego.humano.setResultado("Piedra")
+     juego.maquina.setResultado("Piedra")
+     juego.ganador.should == "Empate"
+   end
+
+ it "Humano elige papel, computadora elige papel, empatan" do
+     juego = Juego.new
+     juego.humano.setResultado("Papel")
+     juego.maquina.setResultado("Papel")
+     juego.ganador.should == "Empate"
+   end
+
+ it "Humano elige tijera, computadora elige tijera, empatan" do
+     juego = Juego.new
+     juego.humano.setResultado("Tijera")
+     juego.maquina.setResultado("Tijera")
+     juego.ganador.should == "Empate"
+   end
+
 
 end
