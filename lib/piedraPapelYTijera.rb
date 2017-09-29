c2db9bdb742e1e5b99279807310b6821d825a864
vagrant <vagrant@kleer-csd-box>
2017-09-29 15:30:06 -0300

    Refactor de metodo ganador, se arma con condiciones de guarda.

--- lib/piedraPapelYTijera.rb
+++ lib/piedraPapelYTijera.rb
@@ -17,24 +17,23 @@ class Juego
    end
 
    def ganador
-      if @humano.resultado == "Tijera" && @maquina.resultado == "Piedra" 	
+      ganaMaquina = @humano.resultado == "Tijera" && @maquina.resultado == "Piedra" || 
+		    @humano.resultado == "Papel" && @maquina.resultado == "Tijera" || 
+		    @humano.resultado == "Piedra" && @maquina.resultado == "Papel"
+      ganaHumano =  @humano.resultado == "Papel" && @maquina.resultado == "Piedra" || 
+		    @humano.resultado == "Tijera" && @maquina.resultado == "Papel" || 
+                    @humano.resultado == "Piedra" && @maquina.resultado == "Tijera"
+      if ganaMaquina	
         return "¡Perdiste!"   
-      elsif @humano.resultado == "Papel" && @maquina.resultado == "Piedra"
- 	@humano.sumarPunto
-        return "¡Ganaste!" 
-      elsif @humano.resultado == "Tijera" && @maquina.resultado == "Papel"
- 	@humano.sumarPunto
-        return "¡Ganaste!"
-      elsif @humano.resultado == "Papel" && @maquina.resultado == "Tijera"
-        return "¡Perdiste!"      
-      elsif @humano.resultado == "Piedra" && @maquina.resultado == "Tijera"
- 	@humano.sumarPunto
+      end
+      if ganaHumano 
+	@humano.sumarPunto
         return "¡Ganaste!"   
-      elsif @humano.resultado == "Piedra" && @maquina.resultado == "Papel"
-        return "¡Perdiste!" 
-      elsif @humano.resultado == @maquina.resultado 
+      end
+      if @humano.resultado == @maquina.resultado 
         return "Empate" 
-      end  
+      end
+      return "Error"
    end
 
    def reiniciar
